f6c39588d748d74327073de37aa5784a94eeeca8
Daniel Jérusalmy <djerusal@uqac.ca>
2025-05-09 07:26:43 -0400

    test

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Link } from "@/i18n/navigation";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import { Book, Menu, X } from "lucide-react";
